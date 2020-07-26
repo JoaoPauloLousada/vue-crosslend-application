297cb8a89658699da538c28ce4afc3891d8aee06
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-07-26 20:59:41 -0300

    Adjust footer

---
+++ src/components/Footer/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="footer container-fluid text-white bg-primary py-2 py-md-0">
+    <div class="row h-100">
+      <div class="col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-end pr-md-0">
+        <span><a href="#" class="text-white">Copyrights</a></span>
+        <span class="px-2">|</span>
+        <span><a href="#" class="text-white">All rights reserved</a></span>
+        <span class="d-none d-md-inline px-2">|</span>
+      </div>
+      <div class="col-12 col-md-6 d-flex align-items-center justify-content-center justify-content-md-start pl-md-0">
+        <span><a href="#" class="text-white">Imprint</a></span>
+        <span class="px-2">|</span>
+        <span><a href="#" class="text-white">Terms of use</a></span>
+        <span class="px-2">|</span>
+        <span><a href="#" class="text-white">Privacy policy</a></span>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.footer {
+  height: auto;
+  font-size: .75rem;
+  @include media-breakpoint-up(md) {
+    height: 2.5rem;
+  }
+}
+</style>
--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="min-vh-100">
+  <div class="vh-100 d-flex flex-column">
     <Navbar />
     <Spinner :show="isLoading" />
-    <div class="container-fluid" v-show="!isLoading">
-      <div class="row">
+    <div class="container-fluid content">
+      <div class="row" v-show="!isLoading">
         <div class="col">
           <slot></slot>
         </div>
       </div>
     </div>
+    <Footer />
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import Navbar from './Navbar'
+import Footer from './Footer'
 import Spinner from './Spinner'
 
 export default {
   components: {
     Navbar,
+    Footer,
     Spinner
   },
   computed: {
@@ -30,6 +33,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.content {
+  flex: 1;
+}
 </style>
