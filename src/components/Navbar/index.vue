3afb945eae1dae6cb9577c3799ea4accd0c54a56
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-07-26 16:42:11 -0300

    update Navbar

--- src/components/Navbar/index.vue
+++ src/components/Navbar/index.vue
@@ -1,25 +1,26 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-0">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button class="navbar-toggler border-0 text-white" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- <span class="navbar-toggler-icon"></span> -->
+      <Menu />
     </button>
 
-    <div class="collapse navbar-collapse h-100 pr-5 border-right" id="navbarSupportedContent">
-      <ul class="navbar-nav align-items-center ml-auto h-100">
+    <div class="collapse navbar-collapse h-100" id="navbarSupportedContent">
+      <ul class="navbar-nav align-items-center ml-auto h-100 pr-5 border-right">
         <li class="nav-item active h-100 d-flex align-items-center" v-for="item in menu.items" :key="item.id">
           <router-link class="nav-link w-100 h-100 d-flex align-items-center" :to="item.route">{{item.name}}</router-link>
         </li>
       </ul>
-    </div>
 
-    <div class="d-none d-md-flex text-light h-100 align-items-center">
-      <div class="avatar">
-        <img src="@/assets/images/avatar.png" class="rounded-circle h-100" alt="user avatar">
-      </div>
-      <div class="d-flex flex-column justify-content-center">
-        <span>{{user | firstName}}</span>
-        <span>{{user | lastName}}</span>
+      <div class="d-none d-lg-flex text-light h-100 align-items-center">
+        <div class="avatar">
+          <img src="@/assets/images/avatar.png" class="rounded-circle h-100" alt="user avatar">
+        </div>
+        <div class="d-flex flex-column justify-content-center">
+          <span>{{user | firstName}}</span>
+          <span>{{user | lastName}}</span>
+        </div>
       </div>
     </div>
   </nav>
@@ -27,8 +28,12 @@
 
 <script>
 import { mapState } from 'vuex'
+import Menu from 'vue-material-design-icons/Menu.vue'
 
 export default {
+  components: {
+    Menu
+  },
   computed: {
     ...mapState({
       menu: state => state.ui.menu,
